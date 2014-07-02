ce45de10f4ea7d225fa27d346f23bf0c9d340979
gagoi <harrygag@gmail.com>
2014-07-02 14:26:08 +0200

    Things ^^

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -2,6 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Simplifier s = new Simplifier(100, 10);
+//		Simplifier s = new Simplifier(100, 10);
+		System.out.println("------------------------------");
+		SimplifierV2 s2 = new SimplifierV2(1250, 10);
 	}	
 }
