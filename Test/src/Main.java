e0a1709defc77c456fadcec14d2aea6bd086bc5a
gagoi <harrygag@gmail.com>
2014-07-02 14:38:58 +0200

    I know that doesn't work

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 //		Simplifier s = new Simplifier(100, 10);
 		System.out.println("------------------------------");
-		SimplifierV2 s2 = new SimplifierV2(1250, 10);
+		SimplifierV2 s2 = new SimplifierV2(250, 1250);
+		System.out.println(s2.getReponse());
 	}	
 }
--- Test/src/SimplifierV2.java
+++ Test/src/SimplifierV2.java
@@ -4,10 +4,7 @@ public class SimplifierV2 {
 	private float x, y;
 	private int[] nbPrime = { 31, 29, 23, 19, 17, 13, 11, 7, 5, 3, 2 };
 	private int i = nbPrime.length - 1;
-	private boolean isSimplified = false; // It would certainly be removed
-											// after;
-	private ArrayList<Integer> test = new ArrayList<Integer>();
-	private int xFinal, yFinal;
+	private int xFinal, yFinal, intFinal;
 
 	// x = numerateur && y = denominateur
 	public SimplifierV2(float x, float y) {
@@ -15,31 +12,42 @@ public class SimplifierV2 {
 		this.y = y;
 		float xTemp, yTemp;
 
-		System.out.println(this.x + " - " + this.y);
-		while (!isSimplified) {
-			for (int i = 0; i <= this.i; i++) {
-				xTemp = this.x / nbPrime[i];
-				yTemp = this.y / nbPrime[i];
-				System.out.println("i=" + i + "  |  nbPrime : " + nbPrime[i] + "  |  x Temp : " + xTemp + "  |  y temp : " + yTemp);
-				if (xTemp == (int) xTemp && yTemp == (int) yTemp) {
-					test.add(i);
-					this.i = i;
-					this.xFinal = (int) xTemp;
-					this.yFinal = (int) yTemp;
-					break;
-				}
-				
-			}
-			isSimplified = true;
+		if (canBeDivided()) {
+			intFinal = (int) (this.x / this.y);
+			return;
 		}
 
-		Object[] testToArray = test.toArray();
-		for (int i = 0; i < testToArray.length; i++) {
-			System.out.println("i = " + testToArray[i] + " - nbPrime =" + nbPrime[(int) testToArray[i]]);
+		System.out.println(this.x + " - " + this.y);
+		for (int i = 0; i <= this.i; i++) {
+			xTemp = this.x / nbPrime[i];
+			yTemp = this.y / nbPrime[i];
+			System.out.println("i=" + i + "  |  nbPrime : " + nbPrime[i] + "  |  x Temp : " + xTemp + "  |  y temp : " + yTemp);
+			if (xTemp == (int) xTemp && yTemp == (int) yTemp) {
+				this.i = i;
+				this.xFinal = (int) xTemp;
+				this.yFinal = (int) yTemp;
+				break;
+			}
+
 		}
-		
-		
-		System.out.println("X final = " + this.xFinal + " | Y final = " + this.yFinal);
 	}
 
+	private boolean canBeDivided() {
+		float test = this.x / this.y;
+		if (test == (int) test)
+			return true;
+		else
+			return false;
+	}
+
+	public String getFinal() {
+		return (xFinal + " - " + yFinal + " - " + intFinal);
+	}
+
+	public String getReponse() {
+		if (intFinal == 0)
+			return (xFinal + " - " + yFinal);
+		else
+			return intFinal + "";
+	}
 }
